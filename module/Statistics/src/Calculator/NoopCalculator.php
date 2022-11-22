4e71905212935c654bcf4dd1d7f17c636119f9a2
Piotr <12448890+Kropidnicki@users.noreply.github.com>
2022-11-22 16:05:05 +0000

    Update NoopCalculator.php

--- module/Statistics/src/Calculator/NoopCalculator.php
+++ module/Statistics/src/Calculator/NoopCalculator.php
@@ -7,44 +7,21 @@ namespace Statistics\Calculator;
 use SocialPost\Dto\SocialPostTo;
 use Statistics\Dto\StatisticsTo;
 
-/**
- * Class TotalPosts
- *
- * @package Statistics\Calculator
- */
 class NoopCalculator extends AbstractCalculator
 {
-
-    protected const UNITS = 'posts';
-
     /**
-     * @var int
-     * @var array
-     */
-    private $totals = 0;
-    private $users = [];
-
-    /**
-     * @param SocialPostTo $postTo
+     * @inheritDoc
      */
     protected function doAccumulate(SocialPostTo $postTo): void
     {
-        $key = $postTo->getAuthorName();
-
-        $this->users[$key] = ($this->users[$key] ?? 0) + 1;
+        // Noops!
     }
 
     /**
-     * @return StatisticsTo
+     * @inheritDoc
      */
     protected function doCalculate(): StatisticsTo
     {
-
-        $stats = new StatisticsTo();
-        foreach ($this->users as $splitPeriod => $total) {
-            $this->totals = $this->totals + $total;
-        }
-
-        return (new StatisticsTo())->setValue($this->totals/count($this->users));
+        return new StatisticsTo();
     }
 }
